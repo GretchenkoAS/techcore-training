f4f027cba85f8a61894bd168ba192de13120ea80
Andrei Hretchanka <gret.andrey@gmail.com>
2025-10-27 12:01:34 +0300

    Task 999 Dependency version management - Added conflict

--- module3-build-systems/build.gradle.kts
+++ module3-build-systems/build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    // Depends on jackson-databind 2.14.0
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
+    // Depends on jackson-databind 2.13.2
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
+
     implementation("com.google.guava:guava:33.5.0-jre")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
