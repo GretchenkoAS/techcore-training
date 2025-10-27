891d888acc23aa4d96bd8ca7d0b056faef8bf332
Andrei Hretchanka <gret.andrey@gmail.com>
2025-10-27 13:56:17 +0300

    Task 999 Exclusion of transitive dependency. Resolved conflicts

--- module3-build-systems/build.gradle.kts
+++ module3-build-systems/build.gradle.kts
@@ -9,16 +9,27 @@ repositories {
     mavenCentral()
 }
 
+configurations.all {
+    resolutionStrategy {
+        failOnVersionConflict()
+    }
+}
+
 dependencies {
-    // Depends on jackson-databind 2.14.0
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
-    // Depends on jackson-databind 2.13.2
-    implementation("org.springframework.boot:spring-boot-starter-web:2.6.7")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.3")
+
+    implementation("org.springframework.boot:spring-boot-starter-web:2.6.7") {
+        exclude(group = "com.fasterxml.jackson.core", module = "jackson-databind")
+        exclude(group = "com.fasterxml.jackson.core", module = "jackson-core")
+        exclude(group = "com.fasterxml.jackson.core", module = "jackson-annotations")
+        exclude(group = "com.fasterxml.jackson.dataformat", module = "jackson-dataformat-xml")
+        exclude(group = "com.fasterxml.jackson.datatype", module = "jackson-datatype-jdk8")
+        exclude(group = "com.fasterxml.jackson.datatype", module = "jackson-datatype-jsr310")
+        exclude(group = "com.fasterxml.jackson.module", module = "jackson-module-parameter-names")
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
 
     implementation("com.google.guava:guava:33.5.0-jre")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
