b8e0a7e15b272858d4d0ba45431db36be1549947
Andrei Hretchanka <gret.andrey@gmail.com>
2025-10-27 11:18:10 +0300

    Task 999 Adding an external dependency - Google Guava

--- module3-build-systems/src/main/java/org/example/Main.java
+++ module3-build-systems/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import com.google.common.base.Strings;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        String nullString = null;
+        String notNullString = "Andrew";
+        System.out.println("is null or empty: " + Strings.isNullOrEmpty(nullString));
+        System.out.println("is null or empty: " + Strings.isNullOrEmpty(notNullString));
     }
 }
