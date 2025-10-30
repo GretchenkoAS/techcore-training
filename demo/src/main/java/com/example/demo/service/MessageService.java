506e32099615684157a905cdec97438994a5b9b3
Andrei Hretchanka <ahretchanka@costengineering.eu>
2025-10-30 10:57:19 +0300

    Task 1001 Reading properties from application.properties

--- demo/src/main/java/com/example/demo/service/MessageService.java
+++ demo/src/main/java/com/example/demo/service/MessageService.java
@@ -1,11 +1,15 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
 
+    @Value("${app.greeting}")
+    private String greeting;
+
     public String getMessage() {
-        return "Andrei Hretchanka";
+        return greeting;
     }
 }
