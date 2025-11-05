78c8f569b5d69545f1904c4625bc5704101a7be0
Andrei Hretchanka <ahretchanka@costengineering.eu>
2025-11-05 12:28:49 +0300

    Task 1011 Basic security configuration. Configure SecurityFilterChain

---
+++ demo/demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST,"/api/auth/**").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .httpBasic(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
