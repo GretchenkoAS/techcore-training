0eda970d7485723acd137c88ac8225a2487e96d4
Andrei Hretchanka <ahretchanka@costengineering.eu>
2025-11-08 12:46:47 +0300

    Task 1011 Added role support

--- demo/demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public String getHello() {
         return "Hello  Andrew!";
+    }
+
+    @PostMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminMethod() {
+        return "Only for admin!";
     }
 }
