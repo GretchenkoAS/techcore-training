df1a5a8304098729a61270e83fef0b2b4b3561b0
Andrei Hretchanka <gret.andrey@gmail.com>
2025-12-10 11:46:48 +0300

    Task-1023 Add integration with mongo db

--- notificationservice/src/main/java/com/example/demo/service/NotificationService.java
+++ notificationservice/src/main/java/com/example/demo/service/NotificationService.java
@@ -1,13 +1,28 @@
 package com.example.demo.service;
 
 import com.example.demo.event.BookCreatedEvent;
+import com.example.demo.model.Notification;
+import com.example.demo.repository.NotificationRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
+
+    private final NotificationRepository notificationRepository;
+
+    public NotificationService(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
     @KafkaListener(topics = "book_events", groupId = "notificationservice")
     public void handle(BookCreatedEvent event) {
         System.out.println("!!!!!!!!!!!!!!!!!: " + event);
+        Notification notification = new Notification(event.id(),
+                String.format("New book created: %s (%s)", event.name(), event.author()));
+        notificationRepository.save(notification);
+        System.out.println("!!!!!!!!!!!!!!!!!: BOOK SAVED");
     }
+
+
 }
