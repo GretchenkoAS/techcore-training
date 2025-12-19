9dfc131bad9a940855cb903f2029fdee7d4d6846
Andrei Hretchanka <gret.andrey@gmail.com>
2025-12-19 12:42:38 +0300

    Task-1025 Add config server to eureka server

--- configserver/build.gradle.kts
+++ configserver/build.gradle.kts
@@ -22,7 +22,8 @@ extra["springCloudVersion"] = "2025.1.0"
 
 dependencies {
 	implementation("org.springframework.cloud:spring-cloud-config-server")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
